c25e62c9dca8954937f5702188de427fa48a4db9
Alec Greiwe <adgreiwe@attu4.cs.washington.edu>
2016-12-04 20:11:59 -0800

    lab5 done

--- kern/trap.c
+++ kern/trap.c
@@ -288,6 +288,15 @@ trap_dispatch(struct Trapframe *tf)
 
 	// Handle keyboard and serial interrupts.
 	// LAB 5: Your code here.
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_KBD) {
+		kbd_intr();
+		return;
+	}
+
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_SERIAL) {
+		serial_intr();
+		return;
+	}
 
 	if (tf->tf_trapno == T_SYSCALL) {
 		tf->tf_regs.reg_eax = syscall(tf->tf_regs.reg_eax,
